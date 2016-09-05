372427a0901d9b3442fea9280773b0dda271278b
telsa <telsaiori.git@gmail.com>
2016-09-05 14:50:38 +0800

    refactor comment shbmit function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,13 @@ class CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params)
-    redirect_to post_path(@post)
+    @comment = @post.comments.build(comment_params)
+    if @comment.save
+      redirect_to post_path(@post) 
+    else
+      redirect_to post_path(@post) 
+      
+    end
   end
 
   private
