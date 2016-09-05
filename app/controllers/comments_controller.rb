ed7c81c0c5c49f775a9a79716d0c41f99ec59559
telsa <telsaiori.git@gmail.com>
2016-09-05 15:50:07 +0800

    add delete comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -14,10 +14,18 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @comment = Comment.find(params[:id]) 
+    if @comment.destroy
+      redirect_to post_url(@comment.post), notice: '留言刪除成功'
+    end
+  end
+
   private
   
   def comment_params
     params.require(:comment).permit(:name, :comment)
   end
+
 
 end
