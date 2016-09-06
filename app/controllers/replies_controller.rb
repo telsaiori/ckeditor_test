6cc3a9b58614328c0fd76ad8470cbc5e026dce10
telsa <telsaiori.git@gmail.com>
2016-09-06 18:14:24 +0800

    add show reply function

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,2 +1,20 @@
 class RepliesController < ApplicationController
+
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.find(params[:comment_id])
+    @reply = @comment.replies.build(reply_params)
+    if @reply.save
+      redirect_to post_url(@post)
+    else
+      redirect_to post_url(@post), notice: '回覆失敗'
+    end
+
+  end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:name, :reply)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160905090043) do
+ActiveRecord::Schema.define(version: 20160905103202) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160905090043) do
     t.text     "reply"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "comment_id"
   end
 
   create_table "users", force: :cascade do |t|
