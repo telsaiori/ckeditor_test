47b0793f01d8761b208a1b45639b278f5ea0d876
telsa <telsaiori.git@gmail.com>
2016-09-25 04:15:47 +0800

    change game review layout

--- app/assets/javascripts/raty.js
+++ app/assets/javascripts/raty.js
@@ -17,21 +17,24 @@ $(document).ready(function(){
   });
 
   $('.modal').on('shown.bs.modal', function(){
+     alert('hi');
+
     $('#game-rating').raty({
       path: '/assets/',
       scoreName: 'comment[rating]',
       score: function() {
         return $(this).attr('data-score');
-      }
+      } 
     });
 
-    $('.star-rating').raty({
-      path: '/assets/',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+  });
+  
+  $('.game-rating').raty({
+    path: '/assets/',
+    readOnly: true,
+    score: function() {
+    return $(this).attr('data-score');
+    }
   });
 
 });
