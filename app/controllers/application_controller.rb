efe4ac3b01b52845b7c76074caaea6071f52bc2f
telsa <telsaiori.git@gmail.com>
2016-09-26 19:36:16 +0800

    add helper to auth user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def auth_user
+    unless current_user.try(:is_admin)
+      redirect_to user_facebook_omniauth_authorize_path
+    end
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def is_admin?(user)
+    user.try(:is_admin)
+  end
+
+
+  
 end
