5831c1a116b77f5b742f87289ad75a433443bfcd
telsa <telsaiori.git@gmail.com>
2016-10-12 19:13:09 +0800

    add category_post to before_action auth_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
 
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :auth_user, except: [:index, :show]
+  before_action :auth_user, except: [:index, :show, :category_post]
 
   # GET /posts
   # GET /posts.json
